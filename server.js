e74aa27f3768eac2e9ae4a7bf1b4d95fe8ee2261
jarusified <jarus3001@gmail.com>
2015-04-09 03:37:15 +0530

    changed structure _ just in case

---
+++ server.js
@@ -0,0 +1,17 @@
+var sys = require('sys'),
+    http = require('http'),
+    fs = require('fs'),
+    index;
+ 
+fs.readFile('./index.html', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    index = data;
+});
+ 
+http.createServer(function(request, response) {
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    response.write(index);
+    response.end();
+}).listen(8001);
